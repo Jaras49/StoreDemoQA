8d88d44732766d706cd70b9d1bd1e774caf25f58
Jarosław Godwin <jgodwin@pl.sii.eu>
2018-10-29 16:46:23 +0100

    Order and product impl

---
+++ src/main/java/com/store/model/Order.java
@@ -0,0 +1,44 @@
+package com.store.model;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+
+public final class Order {
+
+    private final List<Product> products;
+
+    public Order(List<Product> products) {
+        this.products = products;
+    }
+
+    public void addProduct (Product product) {
+        products.add(product);
+    }
+
+    public BigDecimal getOrderPrice() {
+        return products.stream()
+                .map(Product::getPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(products, order.products);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(products);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "products=" + products +
+                '}';
+    }
+}
