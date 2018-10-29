d2c8772effd9385fa6ff4fc921861a0fec712968
Jarosław Godwin <jgodwin@pl.sii.eu>
2018-10-29 19:08:52 +0100

    Fixed local variable name which was the same like a field

--- src/main/java/com/store/page/category/product/ProductPage.java
+++ src/main/java/com/store/page/category/product/ProductPage.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.math.BigDecimal;
+
 public class ProductPage extends WebElementManipulator<ProductPage> {
 
     private MenuPage menu;
@@ -19,7 +21,7 @@ public class ProductPage extends WebElementManipulator<ProductPage> {
     @FindBy(css = ".currentprice")
     private WebElement productPrice;
 
-    @FindBy(css = "input[tyle='submit']")
+    @FindBy(css = "input[type='submit']")
     private WebElement addToCartButton;
 
     @FindBy(css = ".alert.addtocart")
@@ -39,7 +41,38 @@ public class ProductPage extends WebElementManipulator<ProductPage> {
         return this;
     }
 
+    public ProductPage addProductXtimes(int timesToAddProduct) {
+        for (int i = 0; i < timesToAddProduct; i++) {
+            addProduct();
+        }
+        return this;
+    }
+
+    public int getNumberOfCartItems() {
+        return Integer.parseInt(numberOfCartItems.getText());
+    }
+
+    public String getProductName() {
+        return productTitle.getText();
+    }
+
+    public BigDecimal getProductPrice() {
+        String productPriceString = this.productPrice.getText().replace("$", "");
+        return new BigDecimal(productPriceString);
+    }
+
     public MenuPage getMenu() {
         return menu;
+    }
+
+    private ProductPage addProduct() {
+        return clickElementAndWaitToBeVisible(addToCartButton, productAddedAlert)
+                .waitForElementTextUpdate(numberOfCartItems, expectedCartText());
+    }
+
+    private String expectedCartText() {
+        int currentCartProducts = Integer.parseInt(numberOfCartItems.getText());
+        int expectedCartProducts = currentCartProducts + 1;
+        return String.valueOf(expectedCartProducts);
     }
 }
