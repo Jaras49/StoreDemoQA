3d7d0192456fe2102db06a69cf071fb220770fd3
Jarosław Godwin <jgodwin@pl.sii.eu>
2018-10-29 19:06:18 +0100

    added wait methods

--- src/main/java/com/store/page/BasePage.java
+++ src/main/java/com/store/page/BasePage.java
@@ -2,7 +2,9 @@ package com.store.page;
 
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class BasePage<T extends BasePage> {
@@ -11,7 +13,7 @@ public abstract class BasePage<T extends BasePage> {
     protected WebDriverWait wait;
     protected Actions actions;
 
-    public BasePage(WebDriver driver, WebDriverWait wait, Actions actions) {
+    BasePage(WebDriver driver, WebDriverWait wait, Actions actions) {
         this.driver = driver;
         this.wait = wait;
         this.actions = actions;
@@ -21,6 +23,25 @@ public abstract class BasePage<T extends BasePage> {
 
     public <M> T assertEquals(M expected, M actual) {
         Assertions.assertEquals(expected,actual );
+        return getThis();
+    }
+
+    public T assertTrue(boolean actual) {
+        Assertions.assertTrue(actual);
+        return getThis();
+    }
+
+    public T assertFalse(boolean actual) {
+        Assertions.assertFalse(actual);
+        return getThis();
+    }
+
+    protected WebElement waitForElementToBeVisible(WebElement element) {
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    protected T waitForElementTextUpdate(WebElement element, String textToBe) {
+        wait.until(ExpectedConditions.textToBePresentInElement(element, textToBe));
         return getThis();
     }
 }
