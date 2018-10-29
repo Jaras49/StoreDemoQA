8db10ba23676f8bff77e87ff9a8c728a42c4a22d
Jarosław Godwin <jgodwin@pl.sii.eu>
2018-10-29 19:08:00 +0100

    added click and wait to be visible method

--- src/main/java/com/store/page/WebElementManipulator.java
+++ src/main/java/com/store/page/WebElementManipulator.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+//TODO LOGGER
 public abstract class WebElementManipulator<T extends WebElementManipulator> extends BasePage<T> {
 
     private static Logger LOG = Logger.getLogger(WebElementManipulator.class.getName());
@@ -17,6 +17,14 @@ public abstract class WebElementManipulator<T extends WebElementManipulator> ext
     protected T hoverOverElement(WebElement element) {
         LOG.info("Hovering over element " + element.getTagName());
         actions.moveToElement(element).perform();
+        return getThis();
+    }
+
+    protected T clickElementAndWaitToBeVisible(WebElement toClick, WebElement toBeVisible) {
+        LOG.info("Clicking element " + toClick.getTagName());
+        toClick.click();
+        LOG.info("Waiting for element to become visible " + toBeVisible.getTagName());
+        waitForElementToBeVisible(toBeVisible);
         return getThis();
     }
 
