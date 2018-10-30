8c9477aa3d3881de06a94e0f0a87eef4a666c60e
Jarosław Godwin <jgodwin@pl.sii.eu>
2018-10-30 17:30:09 +0100

    sendKeys, selectDropdownByVisibleText, clickElementAndWaitToBeInvisible methods added

--- src/main/java/com/store/page/WebElementManipulator.java
+++ src/main/java/com/store/page/WebElementManipulator.java
@@ -4,11 +4,14 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 //TODO LOGGER
 public abstract class WebElementManipulator<T extends WebElementManipulator> extends BasePage<T> {
 
     private static Logger LOG = Logger.getLogger(WebElementManipulator.class.getName());
+
 
     protected WebElementManipulator(WebDriver driver, WebDriverWait wait, Actions actions) {
         super(driver, wait, actions);
@@ -20,11 +23,27 @@ public abstract class WebElementManipulator<T extends WebElementManipulator> ext
         return getThis();
     }
 
+    protected T sendKeys(WebElement element, String keysToSend) {
+        element.sendKeys(keysToSend);
+        return getThis();
+    }
+
+    protected T selectDropdownByVisibleText(WebElement selectElement, String text) throws InterruptedException {
+        new Select(selectElement).selectByVisibleText(text);
+        return getThis();
+    }
+
     protected T clickElementAndWaitToBeVisible(WebElement toClick, WebElement toBeVisible) {
         LOG.info("Clicking element " + toClick.getTagName());
         toClick.click();
         LOG.info("Waiting for element to become visible " + toBeVisible.getTagName());
         waitForElementToBeVisible(toBeVisible);
+        return getThis();
+    }
+
+    protected T clickElementAndWaitToBeInvisible(WebElement toClick, WebElement toBeInvisible) {
+        toClick.click();
+        waitForElementToBeInvisible(toBeInvisible);
         return getThis();
     }
 
