566a8f4d130cb074fc4833a6a965c04434085b3b
Jarosław Godwin <jgodwin@pl.sii.eu>
2018-10-31 01:18:23 +0100

    @AfterEach method now takes screenshot at the end of the test

--- src/test/java/com/store/BaseTest.java
+++ src/test/java/com/store/BaseTest.java
@@ -8,10 +8,14 @@ import com.store.page.menu.MenuPage;
 import org.apache.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 public abstract class BaseTest {
@@ -20,15 +24,17 @@ public abstract class BaseTest {
     private static final String STORE_URL = "http://store.demoqa.com/";
     private static final String PROPERTIES_FILE_NAME = "test.properties";
     private static final String DRIVER_PROPERTY = "driver";
+    private static final String SCREENSHOTS_FIR_PROPERTY = "dir.screenshots";
 
     protected WebDriver driver;
     protected MenuPage menu;
+    private Properties properties;
 
     @BeforeEach
     public void setUp() throws IOException, DriverNotFoundException {
-        Properties p = new Properties();
-        p.load(this.getClass().getClassLoader().getResourceAsStream(PROPERTIES_FILE_NAME));
-        DriverType driverType = DriverType.fromName(p.getProperty(DRIVER_PROPERTY));
+        properties = new Properties();
+        properties.load(this.getClass().getClassLoader().getResourceAsStream(PROPERTIES_FILE_NAME));
+        DriverType driverType = DriverType.fromName(properties.getProperty(DRIVER_PROPERTY));
 
         driver = DriverFactory.getDriver(driverType);
         getLogger().info("INITIALIZING " + driverType.getName() + " DRIVER");
@@ -41,10 +47,18 @@ public abstract class BaseTest {
     }
 
     @AfterEach
-    public void tearDown() {
+    public void tearDown() throws IOException {
         getLogger().info("FINISHED TEST");
         getLogger().info("LAST URL " + driver.getCurrentUrl());
+        takeScreenshot();
+
         driver.quit();
+    }
+
+    private void takeScreenshot() throws IOException {
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        Files.move(screenshot.toPath(), Paths.get(properties.getProperty(SCREENSHOTS_FIR_PROPERTY) + screenshot.getName()));
+        Files.deleteIfExists(screenshot.toPath());
     }
 
     protected abstract Logger getLogger();
